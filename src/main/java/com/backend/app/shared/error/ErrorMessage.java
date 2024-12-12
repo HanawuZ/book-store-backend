1465073ff5ada8cf81530df4edefe6e69c9b3181
HanawuZ <b6302245@gmail.com>
2024-12-12 20:16:51 +0700

    feat: prepare to init micro-svc

--- src/main/java/com/backend/app/shared/error/ErrorMessage.java
+++
@@ -1,8 +0,0 @@
-package com.backend.app.shared.error;
-
-public class ErrorMessage {
-
-  public static String getErrorMessage(Exception exception) {
-    return String.format("Internal server error: %s", exception.getMessage());
-  }
-}
