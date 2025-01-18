cf83fb9826a4f68d23bc1c56fa564c44ce698ed1
HanawuZ <b6302245@gmail.com>
2025-01-18 10:23:47 +0700

    feat: restore spring

---
+++ catalog-svc/src/main/java/com/example/catalog_svc/configs/grpc/GrpcConfiguration.java
@@ -0,0 +1,14 @@
+package com.example.catalog_svc.configs.grpc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class GrpcConfiguration {
+  
+  // @Bean
+  // PingPongImpl pingPongImpl() {
+  //   return new PingPongImpl();
+  // }
+}
